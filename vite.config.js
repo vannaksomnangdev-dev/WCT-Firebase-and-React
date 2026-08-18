2884ad83e20ba0f65afe92a8e8f32bee72b96b66
vannaksomnangdev-dev <vannaksomnang.dev@gmail.com>
2026-08-18 21:55:28 +0700

    done?

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // <-- Change this to relative path
+  base: "/WCT-Firebase-and-React/", // 👈 Ensure this matches your repo name
   server: {
     port: 3000,
   },
